38a493633d9b0b3bf6de0a91dadaff052aea859b
Berny Linville <bernylinville@devopsthink.org>
2023-07-06 18:31:43 +0800

    Create currents.config.ts

---
+++ currents.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+import { cloudPlugin } from "cypress-cloud/plugin";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      return cloudPlugin(on, config);
+    },
+  },
+});
